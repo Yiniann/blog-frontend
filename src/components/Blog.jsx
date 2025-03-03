d31ffc92817925506fd09bf89419f2b2828bf9c2
y1niann <i@en1an.com>
2025-03-03 02:36:46 -0500

    blog-details

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <p>
+        <strong>{blog.title}</strong> 
+        <button onClick={toggleVisibility} style={{ marginLeft: 10 }}>
+          {visible ? 'hide' : 'view'}
+        </button>
+      </p>
+
+      {visible && (
+        <>
+          <p>{blog.url}</p>
+          <p>
+            Like: {blog.likes} <button>like</button>
+          </p>
+          <p>{blog.author}</p>
+        </>
+      )}
+    </div>
+  )
+}
 
 export default Blog
